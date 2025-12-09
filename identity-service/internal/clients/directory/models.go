8a66b0cdd838b5fead16d702cf777678492184b3
Ivan12390123 <i_van_r@mail.ru>
2025-12-09 23:18:16 +0300

    change the string type to uuid type in UserResponse

--- identity-service/internal/clients/directory/models.go
+++ identity-service/internal/clients/directory/models.go
@@ -1,14 +1,18 @@
 package directory
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
 
 type UserResponse struct {
-	UserID     string `json:"user_id"`
-	Email      string `json:"email"`
-	FirstName  string `json:"first_name"`
-	LastName   string `json:"last_name"`
-	IsActive   bool   `json:"is_active"`
-	Department string `json:"department,omitempty"`
+	UserID       uuid.UUID `json:"user_id"`
+	Email        string    `json:"email"`
+	FirstName    string    `json:"first_name"`
+	LastName     string    `json:"last_name"`
+	IsActive     bool      `json:"is_active"`
+	DepartmentID uuid.UUID `json:"department,omitempty"`
 }
 
 var (
