ac0d53bad8621cabd39674f37426bef4412165ca
Ivan12390123 <i_van_r@mail.ru>
2025-12-09 23:20:46 +0300

    update the departmentID type to a reference in the UserResponse structure

--- identity-service/internal/clients/directory/models.go
+++ identity-service/internal/clients/directory/models.go
@@ -7,12 +7,12 @@ import (
 )
 
 type UserResponse struct {
-	UserID       uuid.UUID `json:"user_id"`
-	Email        string    `json:"email"`
-	FirstName    string    `json:"first_name"`
-	LastName     string    `json:"last_name"`
-	IsActive     bool      `json:"is_active"`
-	DepartmentID uuid.UUID `json:"department,omitempty"`
+	UserID       uuid.UUID  `json:"user_id"`
+	Email        string     `json:"email"`
+	FirstName    string     `json:"first_name"`
+	LastName     string     `json:"last_name"`
+	IsActive     bool       `json:"is_active"`
+	DepartmentID *uuid.UUID `json:"department,omitempty"`
 }
 
 var (
